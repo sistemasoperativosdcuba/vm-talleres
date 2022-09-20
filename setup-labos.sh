df5bcbfa0dd00d7b9185b5b50b345f38ddcad2d6
Pablo Montepagano <pablo@montepagano.com.ar>
2022-09-20 12:03:10 -0300

    Rename repo

--- setup-labos.sh
+++ setup-labos.sh
@@ -18,21 +18,21 @@ wget --directory-prefix="$TMP" -N https://github.com/moparisthebest/static-curl/
 mv curl-amd64 "$LOCALBIN/curl"
 chmod +x "$LOCALBIN/curl"
 
-wget --directory-prefix="$TMP" -N https://github.com/sistemasoperativosdcuba/talleres/raw/main/bsdtar
+wget --directory-prefix="$TMP" -N https://github.com/sistemasoperativosdcuba/vm-talleres/raw/main/bsdtar
 mv bsdtar "$LOCALBIN"
 chmod +x "$LOCALBIN/bsdtar"
 
-wget --directory-prefix="$TMP" -N https://github.com/sistemasoperativosdcuba/talleres/raw/main/talleres-box.torrent
-wget --directory-prefix="$TMP" -N https://github.com/sistemasoperativosdcuba/talleres/raw/main/setup-env-talleres
+wget --directory-prefix="$TMP" -N https://github.com/sistemasoperativosdcuba/vm-talleres/raw/main/talleres-box.torrent
+wget --directory-prefix="$TMP" -N https://github.com/sistemasoperativosdcuba/vm-talleres/raw/main/setup-env-talleres
 chmod +x setup-env-talleres
 
 ./setup-env-talleres
 
-wget --directory-prefix="$TMP" -N https://github.com/sistemasoperativosdcuba/talleres/raw/main/labo-box-metadata.json
+wget --directory-prefix="$TMP" -N https://github.com/sistemasoperativosdcuba/vm-talleres/raw/main/labo-box-metadata.json
 vagrant box add labo-box-metadata.json
 
 cd "$PROYECTO"
-wget -O Vagrantfile https://github.com/sistemasoperativosdcuba/talleres/raw/main/Vagrantfile-labos
+wget -O Vagrantfile https://github.com/sistemasoperativosdcuba/vm-talleres/raw/main/Vagrantfile-labos
 
 echo "Listo. Podés ejecutar el comando 'vagrant up' y luego 'vagrant ssh' para abrir una consola en la VM."
 echo "Los archivos que coloques en el directorio $PROYECTO los podrás acceder desde dentro de la VM en el directorio /vagrant"
